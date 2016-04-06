ce71cc92ec71d5527dc558457072564c9a9df1d2
tuananh93 <tuananh93@hotmail.fr>
2016-04-06 13:57:36 +0200

    create preview, still channel bug

--- src/main/java/ijfx/service/preview/PreviewService.java
+++ src/main/java/ijfx/service/preview/PreviewService.java
@@ -19,17 +19,27 @@
  */
 package ijfx.service.preview;
 
+import ij.process.ImageProcessor;
+import ijfx.service.ui.FxImageService;
 import io.scif.gui.AWTImageTools;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import static java.lang.Math.toIntExact;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import net.imagej.Dataset;
+import net.imagej.DatasetService;
 import net.imagej.ImageJService;
 import net.imagej.Position;
+import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
+import net.imagej.display.DataView;
+import net.imagej.display.DatasetView;
+import net.imagej.display.DefaultDatasetView;
 import net.imagej.display.ImageDisplay;
 import net.imagej.display.ImageDisplayService;
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.plugin.Parameter;
@@ -47,39 +57,76 @@ public class PreviewService extends AbstractService implements ImageJService {
     @Parameter
     ImageDisplayService imageDisplayService;
 
-//    @Parameter
-//    ImageDisplay imageDisplay;
+    @Parameter
+    DatasetService datasetService;
+
     public PreviewService() {
     }
 
     public Image getImageDisplay() {
-        
-        Position activePosition = imageDisplayService.getActivePosition();
-        Dataset dataset = imageDisplayService.getActiveDataset();
-        long[] dimension = new long[dataset.numDimensions()-2];
-        activePosition.localize(dimension);
-        RandomAccess<RealType<?>> randomAccess = dataset.randomAccess();
-        long[] position = new long[dataset.numDimensions()];
-        position[0]=0;
-        position[1]=0;
-        System.arraycopy(dimension, 0, position, 2, dimension.length);
-        randomAccess.setPosition(position);
-        int width =(int) dataset.max(0);
-        int height = (int)dataset.max(1);
-        
-        double [][] arrayImage = new double[width][height];
-        for (int x = 0; x != width; x++) {
-            randomAccess.setPosition(x,0);                
-            for (int y = 0; y != height; y++) {
-                randomAccess.setPosition(y,1);
-                arrayImage[x][y] = randomAccess.get().getRealDouble();
-            }
+
+        BufferedImage bufferedImage = datasetToBufferedImage(getPreviewDataset(200, 200, 0, 0));
+
+        WritableImage wi = new WritableImage(bufferedImage.getWidth(), bufferedImage.getHeight());
+        SwingFXUtils.toFXImage(bufferedImage, wi);
+        return wi;
+    }
+
+    public Dataset getEmptyDataset(Dataset input, int width, int height) {
+        AxisType[] axisType = new AxisType[input.numDimensions()];
+        CalibratedAxis[] axeArray = new CalibratedAxis[input.numDimensions()];
+        input.axes(axeArray);
+
+        long[] dims = new long[axeArray.length];
+        for (int i = 0; i < dims.length; i++) {
+            axisType[i] = axeArray[i].type();
+            dims[i] = toIntExact(input.max(i) + 1);
 
         }
-        WritableImage wi = new WritableImage(width, height);
-        BufferedImage bufferedImage = AWTImageTools.makeImage(arrayImage, width, height);
-        SwingFXUtils.toFXImage(bufferedImage,wi);
-        return wi;
+        dims[0] = width;
+        dims[1] = height;
+        return datasetService.create(dims, input.getName(), axisType, input.getValidBits(), input.isSigned(), false);
+    }
+
+    public < T extends RealType< T>> Dataset getPreviewDataset(int width, int height, int x, int y) {
+
+        Position activePosition = imageDisplayService.getActivePosition();
+
+        Dataset datasetOrigin = imageDisplayService.getActiveDataset();
+        Dataset datasetOutput = getEmptyDataset(datasetOrigin, width, height);
+
+        long[] dimension = new long[datasetOrigin.numDimensions() - 2];
+        activePosition.localize(dimension);
+
+        long[] position = new long[datasetOrigin.numDimensions()];
+        position[0] = 0;
+        position[1] = 0;
+        System.arraycopy(dimension, 0, position, 2, dimension.length);
+        RandomAccess<T> randomAccessOrigin = (RandomAccess<T>) datasetOrigin.randomAccess();
+        RandomAccess<T> randomAccessOutput = (RandomAccess<T>) datasetOutput.randomAccess();
+        randomAccessOrigin.setPosition(position);
+
+        for (int i = x; i < width + x; i++) {
+            randomAccessOrigin.setPosition(i, 0);
+            randomAccessOutput.setPosition(i - x, 0);
+
+            for (int j = y; j < height + y; j++) {
+                randomAccessOrigin.setPosition(j, 1);
+                randomAccessOutput.setPosition(j - y, 1);
+                randomAccessOutput.get().set(randomAccessOrigin.get());
+            }
+        }
+
+        return datasetOutput;
+    }
+
+    public BufferedImage datasetToBufferedImage(Dataset dataset) {
+
+        final DatasetView view = (DatasetView) imageDisplayService.createDataView(dataset);
+        Position activePosition = imageDisplayService.getActivePosition();
+        view.setPosition(activePosition);
+        view.rebuild();
+        return view.getScreenImage().image();
     }
 
 }
