503571c7343d0f28e4a90d6554aa7eb86c442691
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-04-07 16:08:11 +0200

    Fixed menu integration

--- src/main/java/ijfx/plugins/commands/SaveOverlays.java
+++ src/main/java/ijfx/plugins/commands/SaveOverlays.java
@@ -36,7 +36,7 @@ import org.scijava.ui.UIService;
  *
  * @author cyril
  */
-@Plugin(type = Command.class, menuPath = "Image > Overlays > Save Overlays")
+@Plugin(type = Command.class, menuPath = "Image > Overlay > Save Overlays")
 public class SaveOverlays implements Command {
 
     @Parameter(type = ItemIO.BOTH)
