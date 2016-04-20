cd26449170548a0792a3c763232212dec58709c5
tuananh93 <tuananh93@hotmail.fr>
2016-04-20 11:09:33 +0200

    correct path file

--- src/test/java/ijfx/core/project/MetadataExtractionTest.java
+++ src/test/java/ijfx/core/project/MetadataExtractionTest.java
@@ -99,7 +99,7 @@ public class MetadataExtractionTest extends BaseSciJavaTest {
 
         Timer t = timerService.getTimer("Dataset open benchmarking");
         //String testFile = "/Users/cyril/test_img/psfj/gfp_nikon/gfp_nikon1_2048x2048.tif";
-        String testFile = "/Users/cyril/test_img/jasmin/Sec63cherry GFPPho8truncHDEL/Sec63cherry GFPPho8truncHDEL 1x 1b.tif";
+        String testFile = MetadataExtractionTest.testFile.getPath();//OMG!!!!"/Users/cyril/test_img/jasmin/Sec63cherry GFPPho8truncHDEL/Sec63cherry GFPPho8truncHDEL 1x 1b.tif";
         for (int i = 0; i != 100; i++) {
             t.start();
             Dataset dataset = imagePlaneService.extractPlane(new File(testFile), new long[]{0});
