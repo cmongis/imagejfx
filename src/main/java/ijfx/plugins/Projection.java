906ae1a7273985d2301ba6c35c4a210820c0bd01
tuananh93 <tuananh93@hotmail.fr>
2016-04-28 16:51:32 +0200

    delete comments

--- src/main/java/ijfx/plugins/Projection.java
+++ src/main/java/ijfx/plugins/Projection.java
@@ -19,25 +19,16 @@
  */
 package ijfx.plugins;
 
-import ijfx.plugins.stackconverter.DimensionConverter;
 import static java.lang.Math.toIntExact;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mongis.ndarray.NDimensionalArray;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
-import net.imagej.ops.Op;
-import net.imagej.ops.OpService;
-import net.imagej.ops.Ops;
-import net.imagej.ops.Ops.Math.Add;
-import net.imagej.ops.math.RealMath;
-import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
@@ -55,15 +46,6 @@ import org.scijava.plugin.Plugin;
     @Attr(name = "no-legacy")})
 public class Projection implements Command {
 
-    public static final String MAX = "Max";
-    public static final String MIN = "Max";
-    public static final String AVERAGE = "Average";
-    public static final String SD = "Standard deviation";
-    public static final String SUM = "Sum";
-
-    @Parameter
-    OpService opService;
-
     @Parameter
     DatasetService datasetService;
 
@@ -73,9 +55,6 @@ public class Projection implements Command {
     @Parameter(type = ItemIO.OUTPUT)
     Dataset datasetOutput;
 
-    @Parameter(label = "Type of projection", choices = {MAX, MIN, AVERAGE, SUM, SD})
-    String projectionType;
-
     @Parameter
     ProjectionMethod projectionMethod;
 
@@ -84,7 +63,6 @@ public class Projection implements Command {
 
     @Override
     public void run() {
-        ProjectionMethod projectionMethod = new MinProjection();
         AxisType[] axisType = new AxisType[dataset.numDimensions()];
         CalibratedAxis[] axeArray = new CalibratedAxis[dataset.numDimensions()];
         dataset.axes(axeArray);
@@ -92,65 +70,59 @@ public class Projection implements Command {
         for (int i = 0; i < dims.length; i++) {
             axisType[i] = axeArray[i].type();
             dims[i] = toIntExact(dataset.max(i) + 1);
-            if (i == 3) {
+            if (i == dataset.dimensionIndex(axisTypeParameter)) {
                 dims[i] = 1;
             }
         }
         datasetOutput = datasetService.create(dims, dataset.getName(), axisType, dataset.getValidBits(), dataset.isSigned(), false);
 
-        copyDataset(dataset, datasetOutput, projectionMethod);
+        project(dataset, datasetOutput, projectionMethod);
     }
 
-    public < T extends RealType< T>> void copyDataset(Dataset input, Dataset output, ProjectionMethod projectionMethod) {
+    public < T extends RealType<T>> void project(Dataset input, Dataset output, ProjectionMethod projectionMethod) {
+        int indexToModify = input.dimensionIndex(axisTypeParameter);
         Img<?> inputImg = input.getImgPlus().getImg();
-        long[] dims = new long[5];
+        long[] dims = new long[input.numDimensions()];
         input.dimensions(dims);
 
         RandomAccess<T> randomAccess = (RandomAccess<T>) inputImg.randomAccess();
         RandomAccess<T> randomAccessOutput = (RandomAccess<T>) output.randomAccess();
         long[] position = new long[input.numDimensions()];
-        long[] positionOutput = new long[output.numDimensions()];
-        List<T> list = new ArrayList<>();
-        NDimensionalArray nDimensionalArray = new NDimensionalArray(Arrays.copyOfRange(dims, 2, dims.length));
+        List<T> listToProcess = new ArrayList<>();
+        
+        //In order to avoid creation of too big array
+        long[] dimensionToGenerate = Arrays.copyOfRange(dims, 2, dims.length);
+        
+        //Set the dimension to 1 in order to reduce the size of the array
+        //Cannot set to 0
+        dimensionToGenerate[indexToModify-2] = 1;
+        NDimensionalArray nDimensionalArray = new NDimensionalArray(dimensionToGenerate);
         long[][] possibilities = nDimensionalArray.getPossibilities();
 
         for (int x = 0; x < dims[input.dimensionIndex(Axes.X)]; x++) {
             for (int y = 0; y < dims[input.dimensionIndex(Axes.Y)]; y++) {
                 for (long[] possibilitie : possibilities) {
-                    for (int m = 0; m < dims[input.dimensionIndex(axisTypeParameter)]; m++) {
-                        //                        for (long e = 0; e < dims[3]; e++) {
+                    
+                    //Go throug th dimension to project
+                    for (int m = 0; m < dims[indexToModify]; m++) {
                         position[0] = x;
                         position[1] = y;
-                        System.arraycopy(possibilitie, 0, position, 2, possibilities.length);
-                        position[input.dimensionIndex(axisTypeParameter)] = m;
-//                            position[3] = e;
-//                            position[4] = c;
-//                            T tmp;
-//                            try {
-//                                tmp = (T) randomAccess.get().getClass().newInstance();
-//                            } catch (InstantiationException ex) {
-//                                Logger.getLogger(Projection.class.getName()).log(Level.SEVERE, null, ex);
-//                            } catch (IllegalAccessException ex) {
-//                                Logger.getLogger(Projection.class.getName()).log(Level.SEVERE, null, ex);
-//                            }
+                        System.arraycopy(possibilitie, 0, position, 2, possibilitie.length);
+                        position[indexToModify] = m;
+
                         randomAccess.setPosition(position);
 
-                        //tmp = randomAccess.get().copy();
-                        list.add(randomAccess.get().copy());
+                        listToProcess.add(randomAccess.get().copy());
                         if (m == 0) {
                             randomAccessOutput.setPosition(position);
-
-                        } else if (m == dims[input.dimensionIndex(axisTypeParameter)] - 1) {
-                            randomAccessOutput.get().set(projectionMethod.process(list));
-
-                            list.removeAll(list);
+                        } 
+                        else if (m == dims[indexToModify] - 1) {
+                            projectionMethod.process(listToProcess,randomAccessOutput);
+                            listToProcess.removeAll(listToProcess);
                         }
-//                        }
                     }
                 }
             }
         }
-
     }
-
 }
