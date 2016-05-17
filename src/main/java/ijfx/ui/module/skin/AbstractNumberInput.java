1cb5d6c613ee511c835f7efc1e97b9eb39cc828e
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-05-17 10:24:25 +0200

    Added a new input skin allowing the user to select an interval of an dimension

---
+++ src/main/java/ijfx/ui/module/skin/AbstractNumberInput.java
@@ -0,0 +1,100 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.ui.module.skin;
+
+import ijfx.ui.module.input.Input;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+/**
+ *
+ * @author cyril
+ */
+public abstract class AbstractNumberInput<T extends Number> extends AbstractInputSkinPlugin<T> {
+
+    private TextField field;
+
+    public static String CLASS_INVALID = "danger";
+
+    public AbstractNumberInput() {
+        super();
+
+    }
+
+    protected void onKeyTyped(KeyEvent event) {
+        onChange(field.getText());
+    }
+
+    public TextField getTextField() {
+        return field;
+    }
+
+    @Override
+    public Node getNode() {
+
+        return field;
+
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    Property<Integer> value = new SimpleObjectProperty<Integer>();
+
+    abstract T convert(String newValue);
+
+    public void onChange(String newValue) {
+        try {
+            T value = convert(newValue);
+            valueProperty().setValue(value);
+
+            validProperty().setValue(true);
+
+            field.getStyleClass().remove(CLASS_INVALID);
+
+        } catch (Exception e) {
+            if (field.getStyleClass().contains(CLASS_INVALID) == false) {
+
+                validProperty().setValue(false);
+                field.getStyleClass().add(CLASS_INVALID);
+
+            }
+        }
+    }
+
+    @Override
+    public Property<T> valueProperty() {
+        return (ObjectProperty<T>) value;
+    }
+
+    @Override
+    public void init(Input input) {
+        field = new TextField();
+        field.setText(input.getValue().toString());
+        field.addEventHandler(KeyEvent.KEY_RELEASED, this::onKeyTyped);
+    }
+
+}
---
+++ src/main/java/ijfx/ui/module/skin/AxisIntervalInputSkin.java
@@ -0,0 +1,76 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.ui.module.skin;
+
+import ijfx.plugins.AxisInterval;
+import ijfx.plugins.DefaultAxisInterval;
+import ijfx.plugins.LongInterval;
+import ijfx.plugins.RemoveSlice;
+import ijfx.ui.module.InputSkinPlugin;
+import net.imagej.axis.AxisType;
+import net.imagej.display.ImageDisplay;
+import net.imagej.display.ImageDisplayService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = InputSkinPlugin.class)
+public class AxisIntervalInputSkin extends AbstractIntervalInputSkin<AxisInterval> {
+
+    @Parameter
+    ImageDisplayService imageDisplayService;
+
+    @Override
+    protected AxisInterval createInitialInterval() {
+
+        DefaultAxisInterval interval = new DefaultAxisInterval(0, 100);
+        long min, max, low, high;
+
+        ImageDisplay imageDisplay = imageDisplayService.getActiveImageDisplay();
+
+        AxisType axisType = RemoveSlice.getSliceAxis(imageDisplay);
+        if (axisType == null) {
+            min = 0;
+            max = 0;
+            low = min;
+            high = max;
+        } else {
+            int axisIndex = imageDisplay.dimensionIndex(axisType);
+
+            min = imageDisplay.min(axisIndex);
+            max = imageDisplay.max(axisIndex);
+            low = min;
+            high = max;
+
+        }
+
+        return interval;
+    }
+
+    @Override
+    public boolean canHandle(Class<?> clazz) {
+
+        return AxisInterval.class.isAssignableFrom(clazz);
+    }
+
+}
