bc739bc49820d53d337174c85ed89ecbfab307d8
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-06-08 10:10:41 +0200

    Added javadoc and did some refactoring

--- src/main/java/ijfx/service/sampler/DatasetSamplerService.java
+++ src/main/java/ijfx/service/sampler/DatasetSamplerService.java
@@ -28,7 +28,6 @@ import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.sampler.AxisSubrange;
 
-
 import net.imglib2.RandomAccess;
 import net.imglib2.display.ColorTable;
 import net.imglib2.type.numeric.RealType;
@@ -43,30 +42,57 @@ import org.scijava.service.Service;
  * @author cyril
  */
 @Plugin(type = Service.class)
-public class DatasetSamplerService extends AbstractService implements IjfxService{
+public class DatasetSamplerService extends AbstractService implements IjfxService {
 
     @Parameter
     DatasetService datasetService;
 
-    
-    
-    
-    public Dataset isolateDimension(Dataset dataset, AxisType axes, long dimension) {
-        
-        
+    /**
+     * Isolate a point of a dimension of a dataset. It meands for instance that
+     * if you have a dataset with CHANNEL and Z, you can isolate a the Z stack
+     * corresponding to a single channel just by doing :
+     * sampleService.isolateDimension(myDataset,Axes.CHANNEL,2)
+     *
+     * @param dataset the input dataset
+     * @param axes the axe type to isolate
+     * @param position the position in the dimension
+     * @return
+     */
+    public Dataset isolateDimension(Dataset dataset, AxisType axes, long position) {
+
         SamplingDefinition def = new SamplingDefinition(dataset);
-        
-        def.constrain(axes, new AxisSubrange(dimension));
-        
+
+        def.constrain(axes, new AxisSubrange(position));
+
         Dataset output = createOutputImage(dataset, def);
-        
+
         copyData(def, dataset, output);
         return output;
-        
+
     }
-    
-    public Dataset duplicateDate(Dataset dataset, SamplingDefinition def) {
-            
+
+    /**
+     * Extract any subset of an image
+     *
+     * Create a SamplingDefinition object from your input dataset and start
+     * restraining dimension. For instance :
+     * 
+     * SamplingDefinition def = new
+     * SamplingDefinition(myDataset); def.constrain(Axes.X, new
+     * AxisSubrange(20,50)); def.constrain(Axes.Y, new AxisSubrange(20,50));
+     * def.contrain(Axes.Channel, new AxisSubrange(2));
+     *
+     * Dataset output = duplicateData(myDataset,def);
+     *
+     * Here I just extracted the subvolume of the 3 third channel along the z
+     * axis;
+     *
+     * @param dataset the input dataset
+     * @param def Sampling definition defining the sub part to be extracted
+     * @return
+     */
+    public Dataset duplicateData(Dataset dataset, SamplingDefinition def) {
+
         Dataset output = createOutputImage(dataset, def);
         copyData(def, dataset, output);
         return output;
@@ -101,47 +127,42 @@ public class DatasetSamplerService extends AbstractService implements IjfxServic
 
         return output;
     }
-    
-    
+
     private void copyData(final SamplingDefinition def, final Dataset input,
-		final Dataset output)
-	{
-		final PositionIterator iter1 = new SparsePositionIterator(def);
-		final PositionIterator iter2 = new DensePositionIterator(def);
-		// TODO - remove evil casts
-		//final Dataset input = (Dataset) def.getDisplay().getActiveView().getData();
-		//final Dataset output = (Dataset) outputImage.getActiveView().getData();
-		final long[] inputDims = Intervals.dimensionsAsLongArray(input);
-		final long[] outputDims = Intervals.dimensionsAsLongArray(output);
-		final RandomAccess<? extends RealType<?>> inputAccessor =
-			input.getImgPlus().randomAccess();
-		final RandomAccess<? extends RealType<?>> outputAccessor =
-			output.getImgPlus().randomAccess();
-		while (iter1.hasNext() && iter2.hasNext()) {
+            final Dataset output) {
+        final PositionIterator iter1 = new SparsePositionIterator(def);
+        final PositionIterator iter2 = new DensePositionIterator(def);
+        // TODO - remove evil casts
+        //final Dataset input = (Dataset) def.getDisplay().getActiveView().getData();
+        //final Dataset output = (Dataset) outputImage.getActiveView().getData();
+        final long[] inputDims = Intervals.dimensionsAsLongArray(input);
+        final long[] outputDims = Intervals.dimensionsAsLongArray(output);
+        final RandomAccess<? extends RealType<?>> inputAccessor
+                = input.getImgPlus().randomAccess();
+        final RandomAccess<? extends RealType<?>> outputAccessor
+                = output.getImgPlus().randomAccess();
+        while (iter1.hasNext() && iter2.hasNext()) {
 
-			// determine data positions within datasets
-			final long[] inputPos = iter1.next();
-			final long[] outputPos = iter2.next();
-			inputAccessor.setPosition(inputPos);
-			outputAccessor.setPosition(outputPos);
+            // determine data positions within datasets
+            final long[] inputPos = iter1.next();
+            final long[] outputPos = iter2.next();
+            inputAccessor.setPosition(inputPos);
+            outputAccessor.setPosition(outputPos);
 
-			// copy value
-			final double value = inputAccessor.get().getRealDouble();
-			outputAccessor.get().setReal(value);
+            // copy value
+            final double value = inputAccessor.get().getRealDouble();
+            outputAccessor.get().setReal(value);
 
-			// TODO - notice there is a lot of inefficiency following here.
-			// We are setting color tables once per pixel in image and do a lot of
-			// calculation to figure out what table. This should be done once per
-			// plane.
-			
-			// keep dataset color tables in sync
-			final int inputPlaneNumber = AxisUtils.planeNum(inputDims, inputPos);
-			final ColorTable lut = input.getColorTable(inputPlaneNumber);
-			final int outputPlaneNumber = AxisUtils.planeNum(outputDims, outputPos);
-			output.setColorTable(lut, outputPlaneNumber);
-		}
-		
+            // TODO - notice there is a lot of inefficiency following here.
+            // We are setting color tables once per pixel in image and do a lot of
+            // calculation to figure out what table. This should be done once per
+            // plane.
+            // keep dataset color tables in sync
+            final int inputPlaneNumber = AxisUtils.planeNum(inputDims, inputPos);
+            final ColorTable lut = input.getColorTable(inputPlaneNumber);
+            final int outputPlaneNumber = AxisUtils.planeNum(outputDims, outputPos);
+            output.setColorTable(lut, outputPlaneNumber);
+        }
 
-		
-	}
+    }
 }
