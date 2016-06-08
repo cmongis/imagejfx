807e5ce166d4949bb0832025a7aa1357e7dc3e8e
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-06-08 15:31:14 +0200

    Corrected bugs happening when using ChannelMerger and Slice extractor

--- src/main/java/ijfx/plugins/commands/ChannelMerger.java
+++ src/main/java/ijfx/plugins/commands/ChannelMerger.java
@@ -113,6 +113,7 @@ public class ChannelMerger<T extends RealType<T>> extends ContextCommand {
         initializeOutputDataset();
         initializeConverter();
         if (inputMonoChannel) {
+
             processMonoChannelInput();
         }
         else processMultiChannelInput();
@@ -228,8 +229,8 @@ public class ChannelMerger<T extends RealType<T>> extends ContextCommand {
 
             // put it in a array
             int[] rgb = new int[]{red, green, blue};
-            if(outputPosition[1] > 400 && outputPosition[0] > 400)
-            System.out.println(Arrays.toString(outputPosition));
+            
+          
             for (int c = 0; c != rgb.length; c++) {
 
                 // now we go from 0 to 2 (rgb) of the output and additionate
