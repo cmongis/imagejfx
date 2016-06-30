342e11c7655922199c9e2d7929c2b4d9f5f21598
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-06-30 08:40:34 +0200

    Added pixel statistics when segmenting an object

--- src/main/java/ijfx/ui/explorer/view/chartview/ChartView.java
+++ src/main/java/ijfx/ui/explorer/view/chartview/ChartView.java
@@ -145,11 +145,39 @@ public class ChartView extends FilterView implements ExplorerView {
         series.setName(node.toString());
 
     }
+    
+    public Data<Number,Number> transform(Explorable explorable) {
+        
+        String xKey = xComboBox.getSelectionModel().getSelectedItem();
+        String yKey = yComboBox.getSelectionModel().getSelectedItem();
+        
+        return new Data<>(explorable.getMetaDataSet().get(xKey).getDoubleValue(),explorable.getMetaDataSet().get(yKey).getDoubleValue());
+    }
 
     /**
      * Perform a clustering algorithm and load the series.
      */
     public void computeItems() {
+        
+        
+        if(xComboBox.getSelectionModel().getSelectedItem() == null) return;
+        if(yComboBox.getSelectionModel().getSelectedItem() == null) return;
+        
+        Series<Number,Number> series = new Series<>();
+        
+        series.getData().addAll(
+                
+                currentItems
+                        .parallelStream()
+                        .map(this::transform)
+                        .collect(Collectors.toList())
+        );
+        
+        scatterChart.getData().clear();
+        scatterChart.getData().add(series);
+       
+        
+        /*
         if (metadatas[0] != null && metadatas[1] != null) {
 
             scatterChart.getData().clear();
@@ -174,7 +202,7 @@ public class ChartView extends FilterView implements ExplorerView {
                 i++;
             }
 
-        }
+        }*/
     }
 
     @Override
