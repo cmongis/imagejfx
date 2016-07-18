0ee661bafe5193efa81d63a09eab1a761bafeb79
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-07-18 13:23:31 +0200

    Created a comparator allowing to order set of keys according to a priority array.

---
+++ src/main/java/ijfx/core/metadata/MetaDataKeyPrioritizer.java
@@ -0,0 +1,60 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.core.metadata;
+
+import java.util.Comparator;
+import java.util.Set;
+
+/**
+ * Orders metadata by priority list
+ *
+ * @author cyril
+ */
+public class MetaDataKeyPrioritizer implements Comparator<String> {
+
+    final Set<String> priority;
+
+    public MetaDataKeyPrioritizer(Set<String> priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public int compare(String s1, String s2) {
+
+        Integer is1 = priorityIndex(priority, s1);
+        Integer is2 = priorityIndex(priority, s2);
+
+        Integer c = s1.compareTo(s2);
+
+        return 100 * (is2 - is1) + c;
+    }
+
+    public int priorityIndex(Set<String> set, String element) {
+        int i = 0;
+        for (String s : set) {
+            if (s.equals(element)) {
+                return 100 - i;
+            }
+            i++;
+        }
+        return 0;
+    }
+
+}
