6120a98818c0583bb7d57418a4235eab9c4988c0
trinhtuananh <tuananh93@hotmail.fr>
2016-07-25 17:46:47 +0200

    add colortable

--- src/main/java/ijfx/plugins/stack/ImagesToStack.java
+++ src/main/java/ijfx/plugins/stack/ImagesToStack.java
@@ -53,8 +53,8 @@ public class ImagesToStack extends ContextCommand {
     @Parameter
     AxisType axisType;
 
-    @Parameter
-    String title;
+    @Parameter(required = false)
+    String title = "title";
 
     @Override
     public void run() {
@@ -67,33 +67,34 @@ public class ImagesToStack extends ContextCommand {
     public <T extends RealType<T>> void initOutput() {
         int size = datasetArray.length;
         Dataset firstElement = datasetArray[0];
-        
-        long[] dims = new long[firstElement.numDimensions()+1];
+
+        long[] dims = new long[firstElement.numDimensions() + 1];
         firstElement.dimensions(dims);
-        
+
         int axisTypeIndex = 2;
         dims[axisTypeIndex] = size;
-        
+
         AxisType[] axisTypesArray = new AxisType[3];
         IntStream.range(0, firstElement.numDimensions())
                 .forEach(i -> axisTypesArray[i] = firstElement.axis(i).type());
         axisTypesArray[2] = axisType;
         outputDataset = datasetService.create(dims, title, axisTypesArray, firstElement.getValidBits(), firstElement.isSigned(), false);
+        outputDataset.initializeColorTables(firstElement.getColorTableCount());
         outputDataset.setName(title);
     }
-    
-    private void addPlane(Dataset datasetOutput, Dataset input, AxisType axisType, int i){
-        RandomAccess <? extends RealType>randomAccessOutput = datasetOutput.randomAccess();
-        Cursor <? extends RealType>cursorInput = input.cursor();
+
+    private void addPlane(Dataset datasetOutput, Dataset input, AxisType axisType, int i) {
+        RandomAccess<? extends RealType> randomAccessOutput = datasetOutput.randomAccess();
+        Cursor<? extends RealType> cursorInput = input.cursor();
         long[] positionOutput = new long[datasetOutput.numDimensions()];
         while (cursorInput.hasNext()) {
             cursorInput.next();
             cursorInput.localize(positionOutput);
             IntStream.range(0, input.numDimensions())
-                    .forEach(j ->randomAccessOutput.setPosition(cursorInput.getIntPosition(j), j));
+                    .forEach(j -> randomAccessOutput.setPosition(cursorInput.getIntPosition(j), j));
             randomAccessOutput.setPosition(i, 2);
             randomAccessOutput.get().setReal(cursorInput.get().getRealFloat());
-            
+
         }
     }
 
