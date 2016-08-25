fc318f75f3f375b92aeda262050e83e74fa3ce26
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-08-25 10:31:37 +0200

    fixed thumb bug

--- src/main/java/ijfx/ui/plugin/panel/RecentFilePanel.java
+++ src/main/java/ijfx/ui/plugin/panel/RecentFilePanel.java
@@ -136,8 +136,8 @@ public class RecentFilePanel extends BorderPane{
             }
             else {
                 try {
-                    return thumbService.getThumb(file, 0,150, 150);
-                } catch (IOException ex) {
+                    return thumbService.getThumb(file, new long[0],100, 100);
+                } catch (Exception ex) {
                     logService.warn(ex,"Couldn't load file %s",file.getAbsolutePath());
                 }
             }
