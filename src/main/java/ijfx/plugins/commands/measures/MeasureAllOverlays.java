7322fe522767d9ddd75216b7ab811e6f747607d3
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-08-31 08:08:56 +0200

    Added command related to measures

---
+++ src/main/java/ijfx/plugins/commands/measures/MeasureAllOverlays.java
@@ -0,0 +1,49 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.plugins.commands.measures;
+
+import ijfx.service.ui.MeasurementService;
+import net.imagej.display.ImageDisplay;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = Command.class, menuPath="Analyse > Compute object statistics")
+public class MeasureAllOverlays extends ContextCommand{
+
+    @Parameter
+    MeasurementService measurementService;
+    
+    @Parameter(type = ItemIO.BOTH)
+    ImageDisplay imageDisplay;
+    
+    @Override
+    public void run() {
+        
+        measurementService.measureAllOverlay(imageDisplay);
+    }
+    
+}
---
+++ src/main/java/ijfx/plugins/commands/measures/SaveAsCsv.java
@@ -0,0 +1,84 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.plugins.commands.measures;
+
+import ijfx.core.metadata.MetaDataKeyPriority;
+import ijfx.core.metadata.MetaDataSet;
+import ijfx.core.metadata.MetaDataSetUtils;
+import ijfx.ui.datadisplay.object.SegmentedObjectDisplay;
+import ijfx.ui.main.ImageJFX;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+import mongis.utils.TextFileUtils;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.DialogPrompt;
+import org.scijava.ui.UIService;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = Command.class)
+public class SaveAsCsv extends ContextCommand{
+
+    
+    @Parameter(label = "Save as")
+    File outputFile;
+    
+    @Parameter(type = ItemIO.BOTH)
+    SegmentedObjectDisplay objectDisplay;
+    
+    @Parameter
+    UIService uiService;
+    
+    @Override
+    public void run() {
+        
+        if(outputFile == null) {
+            cancel("File set to null");
+            return;
+        }
+        if(isCanceled()) return;
+        
+        List<MetaDataSet> rows = objectDisplay.stream()
+                .map(o->o.getMetaDataSet())
+                .collect(Collectors.toList());
+        
+        String content = MetaDataSetUtils.exportToCSV(rows, ",", true, MetaDataKeyPriority.OBJECT);
+        
+        
+        try {
+            TextFileUtils.writeTextFile(outputFile, content);
+        } catch (IOException ex) {
+           ImageJFX.getLogger().log(Level.SEVERE, null, ex);
+           uiService.showDialog("Error when saving CSV file.", DialogPrompt.MessageType.ERROR_MESSAGE);
+        }
+        
+        
+    }
+    
+}
