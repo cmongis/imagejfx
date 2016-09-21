e1698d302f4f0df3ebb56e203805fdca87001a73
cyril <cyril@GamingPC>
2016-09-21 20:48:53 +0200

    Added the analyse particles command

--- src/main/java/ijfx/ui/segmentation/SegmentationUiPanel.java
+++ src/main/java/ijfx/ui/segmentation/SegmentationUiPanel.java
@@ -19,8 +19,13 @@
  */
 package ijfx.ui.segmentation;
 
+import ijfx.core.metadata.MetaData;
 import ijfx.service.batch.SegmentationService;
+import ijfx.service.batch.SegmentedObject;
+
 import ijfx.service.overlay.OverlayUtilsService;
+import ijfx.service.ui.LoadingScreenService;
+import ijfx.service.ui.MeasurementService;
 import ijfx.service.workflow.Workflow;
 import ijfx.ui.UiConfiguration;
 import ijfx.ui.UiPlugin;
@@ -45,6 +50,7 @@ import mongis.utils.FXUtilities;
 import net.imagej.display.ImageDisplay;
 import net.imagej.display.OverlayService;
 import net.imagej.overlay.BinaryMaskOverlay;
+import net.imagej.overlay.Overlay;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
@@ -72,24 +78,30 @@ public class SegmentationUiPanel extends BorderPane implements UiPlugin {
 
     @Parameter
     private OverlayService overlayService;
-   
+
     @Parameter
     private OverlayUtilsService overlayUtilsService;
-    
+
     @Parameter
     private SegmentationService segmentationService;
-    
+
+    @Parameter
+    private MeasurementService measurementService;
+
+    @Parameter
+    private LoadingScreenService loadingScreenService;
+
     @FXML
     Accordion accordion;
-    
+
     private final Map<TitledPane, SegmentationUiPlugin> nodeMap = new HashMap<>();
 
     private ImageDisplayProperty imageDisplayProperty;
 
     public SegmentationUiPanel() {
-        
+
         try {
-            FXUtilities.injectFXML(this,"/ijfx/ui/segmentation/SegmentationUiPanel.fxml");
+            FXUtilities.injectFXML(this, "/ijfx/ui/segmentation/SegmentationUiPanel.fxml");
             setPrefWidth(200);
             accordion.expandedPaneProperty().addListener(this::onExpandedPaneChanged);
         } catch (IOException ex) {
@@ -110,8 +122,7 @@ public class SegmentationUiPanel extends BorderPane implements UiPlugin {
 
         // when the display is changed, we want to notify only the current wrapper
         imageDisplayProperty.addListener(this::onImageDisplayChanged);
-        
-        
+
         // adding all the UI Plugins
         pluginService
                 .createInstancesOfType(SegmentationUiPlugin.class)
@@ -125,31 +136,29 @@ public class SegmentationUiPanel extends BorderPane implements UiPlugin {
         return nodeMap.get(getExpandedPane());
     }
 
-    
     private void onExpandedPaneChanged(Observable obs, TitledPane oldPane, TitledPane newPane) {
         nodeMap.get(newPane).setImageDisplay(getCurrentImageDisplay());
     }
-    
+
     private void onMaskChanged(Observable obs, Img<BitType> oldMask, Img<BitType> newMask) {
-        
-        if(newMask != null && getCurrentImageDisplay() != null) {
-            
+
+        if (newMask != null && getCurrentImageDisplay() != null) {
+
             new CallbackTask()
-                    .run(()->updateMask(getCurrentImageDisplay(),newMask))
+                    .run(() -> updateMask(getCurrentImageDisplay(), newMask))
                     .start();
-            
+
         }
-        
+
     }
-    
+
     private ImageDisplay getCurrentImageDisplay() {
         return imageDisplayProperty.getValue();
     }
-    
+
     //adds a wrapper
     private void addPlugin(PluginWrapper wrapper) {
-        
-        
+
         wrapper.maskProperty().addListener(this::onMaskChanged);
         // put it in a map with the corresponding plugin
         nodeMap.put(wrapper, wrapper.getPlugin());
@@ -160,59 +169,87 @@ public class SegmentationUiPanel extends BorderPane implements UiPlugin {
     private List<TitledPane> getPanes() {
         return accordion.getPanes();
     }
-    
+
     private TitledPane getExpandedPane() {
         return accordion.getExpandedPane();
     }
-    
+
     // 
     private void onImageDisplayChanged(Observable obs, ImageDisplay oldValue, ImageDisplay newValue) {
-        
-        if(getActivePlugin() != null)
-        getActivePlugin().setImageDisplay(newValue);
+
+        if (getActivePlugin() != null) {
+            getActivePlugin().setImageDisplay(newValue);
+        }
 
     }
 
     private BinaryMaskOverlay getBinaryMask(ImageDisplay imageDisplay) {
         return overlayUtilsService.findOverlayOfType(imageDisplay, BinaryMaskOverlay.class);
     }
-    
-    private BinaryMaskOverlay createBinaryMaskOverlay(ImageDisplay imageDisplay,Img<BitType> mask) {
-        
+
+    private BinaryMaskOverlay createBinaryMaskOverlay(ImageDisplay imageDisplay, Img<BitType> mask) {
+
         BinaryMaskOverlay overlay = new BinaryMaskOverlay(context, new BinaryMaskRegionOfInterest<>(mask));
-        
+
         overlayService.addOverlays(imageDisplay, Arrays.asList(overlay));
-        
+
         return overlay;
-        
+
     }
-    
+
     private synchronized void updateMask(ImageDisplay imageDisplay, Img<BitType> mask) {
-        
+
         BinaryMaskOverlay overlay = getBinaryMask(imageDisplay);
-        if(overlay == null) {
+        if (overlay == null) {
             overlay = createBinaryMaskOverlay(imageDisplay, mask);
-        }
-        else {
-            
+        } else {
+
             BinaryMaskRegionOfInterest regionOfInterest = (BinaryMaskRegionOfInterest) overlay.getRegionOfInterest();
-            
+
             RandomAccessibleInterval<BitType> img = regionOfInterest.getImg();
             RandomAccess<BitType> randomAccess = img.randomAccess();
             Cursor<BitType> cursor = mask.cursor();
-            
+
             cursor.reset();
-            while(cursor.hasNext()) {
+            while (cursor.hasNext()) {
                 cursor.fwd();
                 randomAccess.setPosition(cursor);
                 randomAccess.get().set(cursor.get());
             }
-            
-            
+
         }
         overlay.update();
     }
-    
+
+    @FXML
+    public void analyseParticles() {
+
+        new CallbackTask<Object,Object>()
+                .run((progress,status) -> {
+                    
+                    progress.setStatus("Transforming mask...");
+                    progress.setProgress(1,3);
+                    List<Overlay> overlays = measurementService
+                            .extractOverlays(
+                                    overlayUtilsService
+                                    .findOverlayOfType(getCurrentImageDisplay(), BinaryMaskOverlay.class
+                                    ));
+                    overlayUtilsService.removeAllOverlay(getCurrentImageDisplay());
+                    
+                    overlayUtilsService.addOverlay(getCurrentImageDisplay(), overlays);
+                    progress.setStatus("Measuring...");
+                    progress.setProgress(2,3);
+                    measurementService.measureOverlays(getCurrentImageDisplay(), overlays, this::filterSegmentedObject);
+                    return null;
+                })
+                .submit(loadingScreenService)
+                .start();
+    }
+
+    private boolean filterSegmentedObject(SegmentedObject segmentedObject) {
+        return segmentedObject.getMetaDataSet().getOrDefault(MetaData.LBL_AREA, MetaData.NULL).getDoubleValue() > 8;
+    }
+
     // Wrapping class
     private class PluginWrapper extends TitledPane implements SegmentationUiPlugin {
 
