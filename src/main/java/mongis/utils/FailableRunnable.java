696217876147afe9f75c14842bc563cfdea0d05b
cyril <cyril@47stud.zmbh.uni-heidelberg.de>
2016-11-09 16:40:39 +0100

    Extracted Failable functional interfaces

---
+++ src/main/java/mongis/utils/FailableRunnable.java
@@ -0,0 +1,31 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package mongis.utils;
+
+/**
+ *
+ * @author cyril
+ */
+@FunctionalInterface
+public interface FailableRunnable {
+
+    void run() throws Exception;
+    
+}
